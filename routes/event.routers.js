4479e812f2267d834b4bea80281077c1218fcde4
Suma Shreya T V <sumashreyatv@gmail.com>
2021-05-05 12:37:19 +0530

    Backend server

---
+++ routes/event.routers.js
@@ -0,0 +1,23 @@
+const controllers = require('../controllers/');
+const router = require('express').Router();
+const { auth } = require('../utils');
+
+router.get('/', controllers.event.get.all);
+
+router.get('/details/:id', controllers.event.get.details);
+
+router.post('/create', auth(), controllers.event.post.create);
+
+router.put('/like/:id', auth(), controllers.event.put.like);
+
+router.put('/dislike/:id', auth(), controllers.event.put.dislike);
+
+router.put('/registeruser/:id', auth(), controllers.event.put.registeruser);
+
+router.put('/unregisteruser/:id', auth(), controllers.event.put.unregisteruser);
+
+router.put('/edit/:id', auth(), controllers.event.put.edit);
+
+router.delete('/delete/:id', auth(), controllers.event.delete);
+
+module.exports = router;
